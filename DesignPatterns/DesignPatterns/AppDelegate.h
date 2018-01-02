7fbe143cb4bfdbe583bb0c143adc663ae661d26b
MichaelFan <fan122316@gmail.com>
2018-01-03 07:09:46 +0800

    init project

---
+++ DesignPatterns/DesignPatterns/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DesignPatterns
+//
+//  Created by MichaelFan on 2018/1/3.
+//  Copyright © 2018年 MichaelFan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DesignPatterns/DesignPatterns/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DesignPatterns
+//
+//  Created by MichaelFan on 2018/1/3.
+//  Copyright © 2018年 MichaelFan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
