17b5098d0afb84a458349d7885e5f7f6ec2d5385
MichaelFan <fan122316@gmail.com>
2018-01-29 08:03:22 +0800

    review iterator

---
+++ DesignPatterns/DesignPatterns/NSMutableArray+Stack.h
@@ -0,0 +1,17 @@
+//
+//  NSMutableArray+Stack.h
+//  DesignPatterns
+//
+//  Created by MichaelFan on 2018/1/29.
+//  Copyright © 2018年 MichaelFan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSMutableArray(Stack)
+
+-(void)push:(id)object;
+
+-(id)pop;
+
+@end
---
+++ DesignPatterns/DesignPatterns/NSMutableArray+Stack.m
@@ -0,0 +1,23 @@
+//
+//  NSMutableArray+Stack.m
+//  DesignPatterns
+//
+//  Created by MichaelFan on 2018/1/29.
+//  Copyright © 2018年 MichaelFan. All rights reserved.
+//
+
+#import "NSMutableArray+Stack.h"
+
+@implementation NSMutableArray(Stack)
+
+-(void)push:(id)object
+{
+    
+}
+
+-(id)pop
+{
+    return nil;
+}
+
+@end
