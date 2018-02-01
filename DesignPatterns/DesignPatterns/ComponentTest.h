8a68de09687d4738c9d50c650637eb48d6e9b37f
MichaelFan <fan122316@gmail.com>
2018-02-01 08:25:13 +0800

    component test

---
+++ DesignPatterns/DesignPatterns/ComponentTest.h
@@ -0,0 +1,13 @@
+//
+//  ComponentTest.h
+//  DesignPatterns
+//
+//  Created by MichaelFan on 2018/2/1.
+//  Copyright © 2018年 MichaelFan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ComponentTest : NSObject
+
+@end
---
+++ DesignPatterns/DesignPatterns/ComponentTest.m
@@ -0,0 +1,13 @@
+//
+//  ComponentTest.m
+//  DesignPatterns
+//
+//  Created by MichaelFan on 2018/2/1.
+//  Copyright © 2018年 MichaelFan. All rights reserved.
+//
+
+#import "ComponentTest.h"
+
+@implementation ComponentTest
+
+@end
